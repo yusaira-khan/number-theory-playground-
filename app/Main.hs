9b12aa1ed27f1eedd005183907d5a85fa33775f0
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-09-24 23:27:31 -0400

    Generate rudimentary series
    Cycle ones and generate natural numbers.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Series
 
 main :: IO ()
-main = someFunc
+main = putStrLn (show (take 5 natural))
---
+++ src/Series.hs
@@ -0,0 +1,16 @@
+module Series
+    ( ones,natural,fib, lukas, hamming, interleave, sieve, multiple
+    ) where
+
+ones :: [Int]
+ones = repeat 1
+
+natural :: [Int]
+natural = iterate (+1) 0
+
+fib = id
+lukas = id 
+hamming = id
+interleave = id
+sieve = id 
+multiple = id 
