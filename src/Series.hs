2ca03f0ddae842c4e7c7dd3e717883bd4bce47f4
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-09-25 01:23:25 -0400

    Try to fix hamming

--- src/Series.hs
+++ src/Series.hs
@@ -10,28 +10,34 @@ ones = repeat 1
 natural :: [Int]
 natural = iterate (+1) 0
 
-nextOverlappedNum :: [Int] -> [Int]
-nextOverlappedNum (a : t@(b:_)) = (a+b) : nextOverlappedNum t
+overlappedSum :: [Int] -> [Int]
+overlappedSum (a : t@(b:_)) = (a+b) : overlappedSum t
+
+overlappedProd :: [Int] -> [Int]
+overlappedProd (a : t@(b:_)) = (a*b) : overlappedProd t
 
 --infiniteloops
-nextNonOverlappedNum (a :b: t) = (a+b) : nextNonOverlappedNum (t)
+nextNonOverlapped (a :b: t) = (a+b) : nextNonOverlappedNum (t)
 nextNonOverlappedNum (b:t) = b:nextNonOverlappedNum (b:b:t)
 
 fib :: [Int]
-fib = 1 : 1 : nextOverlappedNum fib
+fib = 1 : 1 : overlappedSum fib
 
 --infinite loops
 fibscousin = 1 : 1 : nextNonOverlappedNum fibscousin
     
     --next (a : t@(b:_)) = ((traceStack (show (a,b))) (a+b)) : next t
 
-lukas = 1 : 3 : nextOverlappedNum lukas
+lukas = 1 : 3 : overlappedSum lukas
 interleave = id
 sieve = id 
 
 subsequentPowers :: Int-> [Int]
 subsequentPowers x =  iterate (x*) 1
 
+subsequentPowersCousin :: Int -> Int  -> [Int]
+subsequentPowersCousin x =  iterate (x*) 
+
 merge:: [Int] -> [Int]-> [Int]
 merge xl@(x:xs) yl@(y:ys) = if (x < y) then (x:(merge xs yl)) else if (x == y) then (x:(merge xs ys)) else y:(merge xl ys)
-hamming = merge (subsequentPowers 2) (subsequentPowers 3)
+hamming =  merge (subsequentPowersCousin 3 1) (subsequentPowersCousin 2 1)
